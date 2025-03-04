cbfb50e8dbd4ed8975428d21abfa5a6ab662ed4f
dedensinurat <deeny3366@gmail.com>
2025-03-04 19:07:13 +0700

    Update PengaturanSeeder.php

--- database/seeders/PengaturanSeeder.php
+++ database/seeders/PengaturanSeeder.php
@@ -15,11 +15,7 @@ class PengaturanSeeder extends Seeder
     {
         Pengaturan::create([
             'name' => 'VokasiTera',
-<<<<<<< HEAD
-            'logo' => 'assets/img//bg/bg1-removebg-preview.png'
-=======
             'logo' => 'assets/img/Logovokasi.png'
->>>>>>> cb0191d309aa5db057211c6eb8edc7a52fbf993b
         ]);
     }
 }
